64c7df0c1599b3c1ffcc4b4a6aac5103761cdca5
Sander Vocke <sandervocke@gmail.com>
2022-09-02 12:30:57 +0000

    Dynamic property binding

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 import sys
+import time
 
-from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtProperty, QTimer
 
+class LoopManager(QObject):
+    lengthChanged = pyqtSignal(float)
+    posChanged = pyqtSignal(float)
+
+    def __init__(self, parent=None):
+        super(LoopManager, self).__init__(parent)
+        self._length = 1.0
+        self._pos = 0.0
+    
+    @pyqtProperty(float, notify=lengthChanged)
+    def length(self):
+        return self._length
+    
+    @length.setter
+    def length(self, l):
+        if self._length != l:
+            self._length = l
+            self.lengthChanged.emit(l)
+    
+    @pyqtProperty(float, notify=posChanged)
+    def pos(self):
+        return self._pos
+    
+    @pos.setter
+    def pos(self, p):
+        if self._pos != p:
+            self._pos = p
+            self.posChanged.emit(p)
+
+def update_loop(mgr):
+    t = time.monotonic()
+    mgr.pos = t % mgr.length
 
 app = QGuiApplication(sys.argv)
 
 engine = QQmlApplicationEngine()
+loop_mgr = LoopManager()
+loop_mgr.length = 10.0
+engine.rootContext().setContextProperty("loop_manager", loop_mgr)
+timer = QTimer(interval=100)
+timer.timeout.connect(lambda: update_loop(loop_mgr))
+timer.start()
 engine.quit.connect(app.quit)
 engine.load('main.qml')
+
+
 
 sys.exit(app.exec())
--- main.qml
+++ main.qml
@@ -3,14 +3,31 @@ import QtQuick.Controls 2.15
 
 ApplicationWindow {
     visible: true
-    width: 600
-    height: 500
-    title: "HelloApp"
+    width: 1000
+    height: 800
+    title: "ShoopDaLoop"
 
-    Text {
-        anchors.centerIn: parent
-        text: "Hello World"
-        font.pixelSize: 24
+    component LoopProgressIndicator : ProgressBar {
+        property double length
+        property double pos
+
+        value: pos / length
+    }
+
+    Column {
+        LoopProgressIndicator {
+            length: loop_manager.length
+            pos: loop_manager.pos
+        }
+        LoopProgressIndicator {
+            length: 100.0
+            pos: 89.0
+        }
+
+        Text {
+            text: "Hello World"
+            font.pixelSize: 24
+        }
     }
 
 }
