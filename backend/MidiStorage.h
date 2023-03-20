55fe104cfe7933c77c6b047a842f00e8c875dc46
Sander Vocke <sandervocke@gmail.com>
2023-03-20 13:46:12 +0000

    WIP

--- backend/MidiStorage.h
+++ backend/MidiStorage.h
@@ -10,92 +10,6 @@
 #include <iostream>
 #include <functional>
 
-template<typename _MidiStorage>
-class MidiStorageCursor {
-    std::optional<size_t> m_offset;
-    std::optional<size_t> m_prev_offset;
-    std::shared_ptr<const _MidiStorage> m_storage;
-    
-    using Elem = typename _MidiStorage::Elem;
-public:
-    MidiStorageCursor(std::shared_ptr<const _MidiStorage> _storage) : m_storage(_storage) {}
-
-    bool valid() const { return m_offset.has_value(); }
-
-    std::optional<size_t> offset() const { return m_offset; }
-    std::optional<size_t> prev_offset() const { return m_prev_offset; }
-
-    void invalidate() {
-        m_offset.reset();
-        m_prev_offset.reset();
-    }
-
-    void overwrite(size_t offset, size_t prev_offset) {
-        m_offset = offset;
-        m_prev_offset = prev_offset;
-    }
-
-    void reset() {
-        if (m_storage->m_n_events == 0) { invalidate(); }
-        else {
-            m_offset = m_storage->m_tail;
-            m_prev_offset = std::nullopt;
-        }
-    }
-
-    Elem *get(size_t raw_offset) const {
-        return m_offset.has_value() ?
-            (Elem*)(&m_storage->m_data.at(raw_offset)) :
-            nullptr;
-    }
-
-    Elem *get() const {
-        return get(m_offset.value());
-    }
-
-    void next() {
-        auto next_offset = m_storage->maybe_next_elem_offset(get());
-        if (next_offset.has_value()) {
-            m_prev_offset = m_offset;
-            m_offset = next_offset;
-        } else {
-            invalidate();
-        }
-    }
-
-    // Iterate a cursor to the point that it points to the
-    // first event after a given time.
-    // Returns the amount of events processed to get to that point.
-    size_t find_time_forward(size_t time) {
-        if (!valid()) { reset(); }
-        if (!valid()) { return 0; }
-        std::optional<size_t> prev = m_offset;
-        size_t n_processed = 0;
-        for (auto next_offset = m_offset,
-             prev = m_prev_offset
-             ;
-             next_offset.has_value()
-             ;
-             prev = next_offset,
-             next_offset = m_storage->maybe_next_elem_offset(m_storage->unsafe_at(next_offset.value())),
-             n_processed++)
-        {
-            Elem* next_elem = m_storage->unsafe_at(next_offset.value());
-            if (next_elem->storage_time >= time) {
-                // Found
-                m_offset = next_offset;
-                m_prev_offset = prev;
-                return n_processed;
-            }
-        }
-
-        // If we reached here, we reached the end. Reset to an invalid
-        // cursor.
-        reset();
-        return n_processed;
-    }
-
-};
 
 // We need a variable-sized struct that also supports interface inheritance.
 // This cannot really be done in C++, so instead we just make a fixed-size
@@ -118,9 +32,7 @@ struct MidiStorageElem : public MidiSortableMessageInterface {
     }
 
     const uint8_t* get_data() const override {
-        auto rval = ((uint8_t*)this) + sizeof(MidiStorageElem<TimeType, SizeType>);
-        std::cout << rval << std::endl;
-        return rval;
+        return ((uint8_t*)this) + sizeof(MidiStorageElem<TimeType, SizeType>);
     }
 
     uint32_t get_time() const override { return proc_time; }
@@ -135,21 +47,21 @@ struct MidiStorageElem : public MidiSortableMessageInterface {
 };
 
 template<typename TimeType, typename SizeType>
-class MidiStorage : public std::enable_shared_from_this<MidiStorage<TimeType, SizeType>>{
-public:
-    typedef MidiStorage<TimeType, SizeType> MyType;
-    typedef MidiStorageCursor<MyType> Cursor;
-    typedef std::shared_ptr<Cursor> SharedCursor;
-    typedef MidiStorageElem<TimeType, SizeType> Elem;
-    friend class MidiStorageCursor<MyType>;
+class MidiStorageCursor;
 
-private:
+template<typename TimeType, typename SizeType>
+class MidiStorageBase : public std::enable_shared_from_this<MidiStorageBase<TimeType, SizeType>> {
+public:
+    using Elem = MidiStorageElem<TimeType, SizeType>;
+    Elem dummy_elem; // Prevents incomplete template type in debug build
+    friend class MidiStorageCursor<TimeType, SizeType>;
+
+protected:
     std::vector<uint8_t> m_data;
     size_t m_tail;
     size_t m_head;
     size_t m_head_start;
     size_t m_n_events;
-    std::vector<std::weak_ptr<Cursor>> m_cursors;
     static constexpr size_t n_starting_cursors = 10;
 
     bool valid_elem_at(size_t offset) const {
@@ -179,81 +91,25 @@ private:
     }
 
     void store_unsafe(size_t offset, TimeType t, SizeType s, const uint8_t* d) {
+        static Elem elem;
+        elem.size = s;
+        elem.proc_time = elem.storage_time = t;
         uint8_t* to = &(m_data.at(offset));
-        Elem* to_elem = (Elem*) to;
-        to_elem->storage_time = t;
-        to_elem->size = s;
+        memcpy((void*)to, (void*)&elem, sizeof(Elem));
         void* data_ptr = to + sizeof(Elem);
         memcpy(data_ptr, d, s);
     }
 
 public:
-    MidiStorage(size_t data_size) :
+    MidiStorageBase(size_t data_size) :
         m_head(0), m_tail(0), m_head_start(0), m_n_events(0),
         m_data(data_size)
-    {
-        m_cursors.reserve(n_starting_cursors);
-    }
-
-    SharedCursor create_cursor() {
-        auto maybe_self = std::enable_shared_from_this<MidiStorage<TimeType, SizeType>>::weak_from_this();
-        if (auto self = maybe_self.lock()) {
-            auto rval = std::make_shared<Cursor>(self);
-            m_cursors.push_back(rval);
-            rval->reset();
-            return rval;
-        } else {
-            throw std::runtime_error("Attempting to create cursor for destructed storage");
-        }
-    }
+    {}
 
     size_t bytes_capacity() const { return m_data.size(); }
 
     void clear() {
-        for (auto &elem: m_cursors) {
-            if(auto c = elem.lock()) {
-                c->invalidate();
-            }
-        }
-        m_cursors.clear();
         m_head = m_tail = m_head_start = m_n_events = 0;
-    }
-
-    // Note: should only be used if caller knows that "bytes" is exactly
-    // at a boundary between messages
-    void truncate(TimeType time) {
-        if (m_n_events > 0 && unsafe_at(m_head_start)->storage_time > time) {
-            auto cursor = create_cursor();
-            if (cursor->valid()) {
-                m_n_events = cursor->find_time_forward(time);
-                m_head = cursor->offset().value();
-                m_head_start = cursor->prev_offset().value_or(0);
-
-                for (auto &cursor: m_cursors) {
-                    std::shared_ptr<Cursor> maybe_shared = cursor.lock();
-                    if(maybe_shared) {
-                        if (maybe_shared->offset() > m_head) { maybe_shared->overwrite(m_head, m_head_start); }
-                    }
-                }
-            }
-        }
-    }
-
-    void copy(MyType &to) const {
-        if (to.m_data.size() < m_data.size()) { to.m_data.resize(m_data.size()); }
-        if(m_head >= m_tail) {
-            memcpy((void*)(to.m_data.data()), (void*)&(m_data.at(m_tail)), m_head - m_tail);
-        } else {
-            size_t first_copy = m_data.size() - m_tail;
-            size_t second_copy = m_head;
-            memcpy((void*)(to.m_data.data()), (void*)&(m_data.at(m_tail)), first_copy);
-            memcpy((void*)(to.m_data.at(first_copy)), (void*)m_data.data(), second_copy);
-        }
-        to.m_tail = 0;
-        to.m_head = bytes_occupied();
-        to.m_n_events = m_n_events;
-        to.m_head_start = m_n_events > 0 ?
-            to.m_head - (m_head - m_head_start) : 0;
     }
 
     size_t bytes_occupied() const {
@@ -313,8 +169,174 @@ public:
         return true;
     }
 
-    void for_each_msg(std::function<void(TimeType t, SizeType s, uint8_t* data)> cb) const {
-        auto maybe_self = std::enable_shared_from_this<MidiStorage<TimeType, SizeType>>::weak_from_this();
+    void copy(MidiStorageBase<TimeType, SizeType> &to) const {
+        if (to.m_data.size() < m_data.size()) { to.m_data.resize(m_data.size()); }
+        if(m_head >= m_tail) {
+            memcpy((void*)(to.m_data.data()), (void*)&(m_data.at(m_tail)), m_head - m_tail);
+        } else {
+            size_t first_copy = m_data.size() - m_tail;
+            size_t second_copy = m_head;
+            memcpy((void*)(to.m_data.data()), (void*)&(m_data.at(m_tail)), first_copy);
+            memcpy((void*)(to.m_data.at(first_copy)), (void*)m_data.data(), second_copy);
+        }
+        to.m_tail = 0;
+        to.m_head = bytes_occupied();
+        to.m_n_events = m_n_events;
+        to.m_head_start = m_n_events > 0 ?
+            to.m_head - (m_head - m_head_start) : 0;
+    }
+};
+
+template<typename TimeType, typename SizeType>
+class MidiStorageCursor {
+public:
+    using Storage = MidiStorageBase<TimeType, SizeType>;
+    using Elem = MidiStorageElem<TimeType, SizeType>;
+    Elem dummy_elem; // Prevents incomplete template type in debug build
+
+private:
+    std::optional<size_t> m_offset;
+    std::optional<size_t> m_prev_offset;
+    std::shared_ptr<const Storage> m_storage;
+
+public:
+    MidiStorageCursor(std::shared_ptr<const Storage> _storage) : m_storage(_storage) {}
+
+    bool valid() const { return m_offset.has_value(); }
+
+    std::optional<size_t> offset() const { return m_offset; }
+    std::optional<size_t> prev_offset() const { return m_prev_offset; }
+
+    void invalidate() {
+        m_offset.reset();
+        m_prev_offset.reset();
+    }
+
+    void overwrite(size_t offset, size_t prev_offset) {
+        m_offset = offset;
+        m_prev_offset = prev_offset;
+    }
+
+    void reset() {
+        if (m_storage->m_n_events == 0) { invalidate(); }
+        else {
+            m_offset = m_storage->m_tail;
+            m_prev_offset = std::nullopt;
+        }
+    }
+
+    Elem *get(size_t raw_offset) const {
+        auto e = (Elem*)(&m_storage->m_data.at(raw_offset));
+        return (Elem*)(&m_storage->m_data.at(raw_offset));
+    }
+
+    Elem *get() const {
+        return  m_offset.has_value() ? get(m_offset.value()) : nullptr;
+    }
+
+    void next() {
+        auto next_offset = m_storage->maybe_next_elem_offset(get());
+        if (next_offset.has_value()) {
+            m_prev_offset = m_offset;
+            m_offset = next_offset;
+        } else {
+            invalidate();
+        }
+    }
+
+    // Iterate a cursor to the point that it points to the
+    // first event after a given time.
+    // Returns the amount of events processed to get to that point.
+    size_t find_time_forward(size_t time) {
+        if (!valid()) { reset(); }
+        if (!valid()) { return 0; }
+        std::optional<size_t> prev = m_offset;
+        size_t n_processed = 0;
+        for (auto next_offset = m_offset,
+             prev = m_prev_offset
+             ;
+             next_offset.has_value()
+             ;
+             prev = next_offset,
+             next_offset = m_storage->maybe_next_elem_offset(m_storage->unsafe_at(next_offset.value())),
+             n_processed++)
+        {
+            Elem* next_elem = m_storage->unsafe_at(next_offset.value());
+            if (next_elem->storage_time >= time) {
+                // Found
+                m_offset = next_offset;
+                m_prev_offset = prev;
+                return n_processed;
+            }
+        }
+
+        // If we reached here, we reached the end. Reset to an invalid
+        // cursor.
+        reset();
+        return n_processed;
+    }
+};
+
+template<typename TimeType, typename SizeType>
+class MidiStorage : public MidiStorageBase<TimeType, SizeType> {
+public:
+    using Elem = MidiStorageElem<TimeType, SizeType>;
+    using Cursor = MidiStorageCursor<TimeType, SizeType>;
+    using SharedCursor = std::shared_ptr<Cursor>;
+
+private:
+    std::vector<std::weak_ptr<Cursor>> m_cursors;
+    static constexpr size_t n_starting_cursors = 10;
+
+public:
+    MidiStorage(size_t data_size) : MidiStorageBase<TimeType, SizeType>(data_size) {
+        m_cursors.reserve(n_starting_cursors);
+    }
+
+    SharedCursor create_cursor() {
+        auto maybe_self = MidiStorageBase<TimeType, SizeType>::weak_from_this();
+        if (auto self = maybe_self.lock()) {
+            auto rval = std::make_shared<Cursor>(self);
+            m_cursors.push_back(rval);
+            rval->reset();
+            return rval;
+        } else {
+            throw std::runtime_error("Attempting to create cursor for destructed storage");
+        }
+    }
+
+    void clear() {
+        for (auto &elem: m_cursors) {
+            if(auto c = elem.lock()) {
+                c->invalidate();
+            }
+        }
+        m_cursors.clear();
+        MidiStorageBase<TimeType, SizeType>::clear();
+    }
+
+    // Note: should only be used if caller knows that "bytes" is exactly
+    // at a boundary between messages
+    void truncate(TimeType time) {
+        if (this->m_n_events > 0 && this->unsafe_at(this->m_head_start)->storage_time > time) {
+            auto cursor = create_cursor();
+            if (cursor->valid()) {
+                this->m_n_events = cursor->find_time_forward(time);
+                this->m_head = cursor->offset().value();
+                this->m_head_start = cursor->prev_offset().value_or(0);
+
+                for (auto &cursor: m_cursors) {
+                    std::shared_ptr<Cursor> maybe_shared = cursor.lock();
+                    if(maybe_shared) {
+                        if (maybe_shared->offset() > this->m_head) { maybe_shared->overwrite(this->m_head, this->m_head_start); }
+                    }
+                }
+            }
+        }
+    }
+
+    void for_each_msg(std::function<void(TimeType t, SizeType s, uint8_t* data)> cb) {
+        auto maybe_self = MidiStorageBase<TimeType, SizeType>::weak_from_this();
         if (auto self = maybe_self.lock()) {
             auto cursor = std::make_shared<Cursor>(self);
             for(cursor->reset(); cursor->valid(); cursor->next()) {           
