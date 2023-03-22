1c3f99e771cce6aea93d91c80e8b0ac83b0300a1
Sander Vocke <sandervocke@gmail.com>
2023-03-22 11:03:01 +0000

    Basic NSM functionality

--- bin/main.py
+++ bin/main.py
@@ -6,21 +6,8 @@ import traceback
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(script_dir + '/..')
 
-from PyQt6.QtGui import QGuiApplication
-from PyQt6.QtQml import QQmlApplicationEngine
-from lib.qml_helpers import register_shoopdaloop_qml_classes
-from lib.q_objects.SchemaValidator import SchemaValidator
-from lib.q_objects.FileIO import FileIO
-
-validator = SchemaValidator()
-file_io = FileIO()
-
-app = QGuiApplication(sys.argv)
-register_shoopdaloop_qml_classes()
-engine = QQmlApplicationEngine()
-engine.quit.connect(app.quit)
-engine.rootContext().setContextProperty("schema_validator", validator)
-engine.rootContext().setContextProperty("file_io", file_io)
-engine.load('{}/../lib/qml/applications/shoopdaloop_main.qml'.format(script_dir))
-
+from lib.q_objects.Application import Application
+app = Application('ShoopDaLoop',
+    '{}/../lib/qml/applications/shoopdaloop_main.qml'.format(script_dir),
+    sys.argv)
 exitcode = app.exec()
