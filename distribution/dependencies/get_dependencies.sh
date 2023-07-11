4d550b3fc655fc89df5ae627916d8e2c94e14147
Sander Vocke <sandervocke@gmail.com>
2023-07-11 09:17:53 +0000

    WIP

--- distribution/dependencies/get_dependencies.sh
+++ distribution/dependencies/get_dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Run as: get_dependencies.sh <dependencies_file>
+# will take the file <dependencies_file>, remove comments and whitespace,
+# and print the rest of the lines as a single line (newlines removed).
+# Result is the list of dependencies as can be used in e.g. a single apt-get command.
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cat $SCRIPTPATH/$1 | grep . | grep -E -v '^[\s]*#' | tr '\n' ' '
