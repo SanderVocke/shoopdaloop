0b1d9ee4362010573962ed56dcf618b7baffd1e9
Sander Vocke <sandervocke@gmail.com>
2023-11-02 12:26:41 +0100

    WIP

--- src/libshoopdaloop/BuildStaticDependencies.cmake
+++ src/libshoopdaloop/BuildStaticDependencies.cmake
@@ -31,11 +31,14 @@ ExternalProject_Add(lv2
     python -m mesonbuild.mesonmain install
 )
 
+find_path (STATIC_PKGCONFIGS
+  NAMES ${STATIC_DEPS_PREFIX}/lib64/pkgconfig/lv2.pc ${STATIC_DEPS_PREFIX}/lib/pkgconfig/lv2.pc)
+
 ExternalProject_Add(serd
   SOURCE_DIR ${CMAKE_SOURCE_DIR}/../third_party/serd
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/serd_build
   CONFIGURE_COMMAND
-    PKG_CONFIG_PATH=${STATIC_DEPS_PREFIX}/lib64/pkgconfig python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
+    PKG_CONFIG_PATH=${STATIC_PKGCONFIGS} python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
   BUILD_COMMAND
     python -m mesonbuild.mesonmain compile
   INSTALL_COMMAND
@@ -47,7 +50,7 @@ ExternalProject_Add(sord
   SOURCE_DIR ${CMAKE_SOURCE_DIR}/../third_party/sord
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/sord_build
   CONFIGURE_COMMAND
-    PKG_CONFIG_PATH=${STATIC_DEPS_PREFIX}/lib64/pkgconfig python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
+    PKG_CONFIG_PATH=${STATIC_PKGCONFIGS} python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
   BUILD_COMMAND
     python -m mesonbuild.mesonmain compile
   INSTALL_COMMAND
@@ -59,7 +62,7 @@ ExternalProject_Add(sratom
   SOURCE_DIR ${CMAKE_SOURCE_DIR}/../third_party/sratom
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/sratom_build
   CONFIGURE_COMMAND
-    PKG_CONFIG_PATH=${STATIC_DEPS_PREFIX}/lib64/pkgconfig python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
+    PKG_CONFIG_PATH=${STATIC_PKGCONFIGS} python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
   BUILD_COMMAND
     python -m mesonbuild.mesonmain compile
   INSTALL_COMMAND
@@ -71,7 +74,7 @@ ExternalProject_Add(lilv
   SOURCE_DIR ${CMAKE_SOURCE_DIR}/../third_party/lilv
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/lilv_build
   CONFIGURE_COMMAND
-    PKG_CONFIG_PATH=${STATIC_DEPS_PREFIX}/lib64/pkgconfig python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
+    PKG_CONFIG_PATH=${STATIC_PKGCONFIGS} python -m mesonbuild.mesonmain setup -Ddefault_library=static --prefix=${STATIC_DEPS_PREFIX} <BINARY_DIR> <SOURCE_DIR>
   BUILD_COMMAND
     python -m mesonbuild.mesonmain compile
   INSTALL_COMMAND
