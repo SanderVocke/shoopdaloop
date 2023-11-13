eab8b3e69303d2e5ec3ce41d6e288014efd98a48
Sander Vocke <sandervocke@gmail.com>
2023-11-13 16:42:50 +0100

    WIP

--- scripts/test_qt_available.py
+++ scripts/test_qt_available.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 from shoopdaloop.lib.ensure_ld_qt import ensure_ld_qt
 
+result = 0
+
 try:
     ensure_ld_qt()
     print("All required Qt libraries are available.")
 except Exception as e:
     print("ShoopDaLoop backend library is not available: {}".format(e))
+    result = 1
 
 try:
     from shoopdaloop.libshoopdaloop_bindings import *
     print("ShoopDaLoop backend library is available.")
 except Exception as e:
     print("ShoopDaLoop backend library is not available: {}".format(e))
+    result = 1
 
 try:
     from shoopdaloop.qml_tests import *
     print("ShoopDaLoop unit test runner is available.")
 except Exception as e:
     print("ShoopDaLoop unit test runner is not available: {}".format(e))
+    result = 1
 
 try:
     from shoopdaloop.qt_extensions import *
     print("ShoopDaLoop Qt extensions are available.")
 except Exception as e:
     print("ShoopDaLoop Qt extensions are not available: {}".format(e))
+    result = 1
+
+exit(result)
