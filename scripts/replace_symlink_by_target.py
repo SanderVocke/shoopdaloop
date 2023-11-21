83e43247c4d1a446145a63bd6242fa385946c25f
Sander Vocke <sander.vocke@itecequipment.com>
2023-11-21 12:26:50 +0100

    wip

---
+++ scripts/replace_symlink_by_target.py
@@ -0,0 +1,13 @@
+#!usr/bin/evn python3
+
+import sys
+import os
+import shutil
+
+def replace_symlink_with_target(filename):
+    if os.path.islink(filename):
+        target_path = os.path.realpath(filename)
+        shutil.copy(target_path, filename)
+        print(f"Replaced symlink {filename} with its target")
+
+replace_symlink_with_target(sys.argv[1])
