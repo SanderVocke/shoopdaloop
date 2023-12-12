646cc4b957fbccf78720acea18b567992b3a8eda
Sander Vocke <sander.vocke@itecequipment.com>
2023-12-12 16:40:02 +0100

    WIP

---
+++ src/libshoopdaloop/test/unit/test_Ports.cpp
@@ -0,0 +1,44 @@
+#include "DummyAudioMidiDriver.h"
+#include <catch2/catch_test_macros.h">
+
+TEST_CASE("Ports - Dummy Audio In - Properties", "[Ports][audio]") {
+    DummyAudioPort port ("dummy", Input);
+
+    CHECK(port->has_internal_read_access());
+    CHECK(!port->has_internal_write_access());
+    CHECK(port->has_implicit_input_source());
+    CHECK(!port->has_implicit_output_sink());
+}
+
+TEST_CASE("Ports - Dummy Audio In - Buffers", "[Ports][audio]") {
+    DummyAudioPort port ("dummy", Input);
+
+    port.PROC_prepare(10);
+    port.PROC_process(10);
+    auto buf = port.PROC_get_buffer(10);
+    CHECK(buf != nullptr);
+    auto buf2 = port.PROC_get_buffer(10);
+    CHECK(buf == buf2);
+
+}
+
+TEST_CASE("Ports - Dummy Audio In - Queue", "[Ports][audio]") {
+    DummyAudioPort port ("dummy", Input);
+    jack_default_audio_sample_t samples[6] = {
+        0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f
+    };
+
+    port.queue_data(6, samples);
+
+    port.PROC_prepare(6);
+    port.PROC_process(6);
+
+    auto buf = port.PROC_get_buffer(6);
+
+    CHECK(buf[0] == Catch::Approx(0.0f));
+    CHECK(buf[1] == Catch::Approx(1.0f));
+    CHECK(buf[2] == Catch::Approx(2.0f));
+    CHECK(buf[3] == Catch::Approx(3.0f));
+    CHECK(buf[4] == Catch::Approx(4.0f));
+    CHECK(buf[5] == Catch::Approx(5.0f));
+}
