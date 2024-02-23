9f6cd0b9d7369b09ff83bb3ff16db469cdb55044
Sander Vocke <sandervocke@gmail.com>
2024-02-23 17:08:07 +0100

    wip

--- distribution/pyinstaller/hook-lupa.py
+++ distribution/pyinstaller/hook-lupa.py
@@ -1,4 +1,4 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
 
 datas = collect_data_files("lupa", True)
-binaries = collect_dynamic_libs("lupa", search_patterns=["*.dll", "*.dylib", "*.so", "*.so*"])
+binaries = collect_dynamic_libs("lupa", search_patterns=["*.dll", "*.dylib", "*.so", "*.so*", "*.pyd"])
