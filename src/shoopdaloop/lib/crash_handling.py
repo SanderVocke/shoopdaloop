ad98d51930ef0eb9e695081d5fcb1c5d9fb23ced
Sander Vocke <sandervocke@gmail.com>
2024-02-27 23:17:30 +0100

    wip

--- src/shoopdaloop/lib/crash_handling.py
+++ src/shoopdaloop/lib/crash_handling.py
@@ -2,6 +2,10 @@ import tempfile
 import faulthandler
 import ctypes
 import os
+import traceback
+
+from PySide6.QtQml import QJSValue
+from shiboken6 import Shiboken
 
 # Keep track of active javascript engines in order to attempt to get tracebacks from them
 g_active_js_engines = set()
@@ -9,17 +13,57 @@ g_active_js_engines = set()
 from shoopdaloop.lib.init_dynlibs import init_dynlibs
 init_dynlibs()
 
+# In the crash callback, attempt to gather additional information about the Python and QML state.
 def crashed_callback(filename):
-    print("Python callback!")
-    print(f"{len(g_active_js_engines)} engines are active")
+    global g_active_js_engines
+    
+    more_info_filename = str(filename + '.info')
+    
+    try:
+        with open(more_info_filename, 'w') as f:
+            f.write("=====================================\n")
+            f.write("Python traceback\n")
+            f.write("=====================================\n\n")
+            traceback.print_stack(file=f)
+            f.write("\n")
+    except Exception as e:
+        pass
+
+    try:
+        js_stacks = []
+        for idx, eng in enumerate(g_active_js_engines):
+            if eng and Shiboken.isValid(eng):
+                eng.throwError("Crash handler stack retrieval helper error")
+                err = eng.catchError()
+                if err:
+                    stack = err.property('stack')
+                    if stack:
+                        stack_str = stack.toString()
+                        js_stacks.append(stack_str)
+        
+        if len(js_stacks) > 0:
+            with open(more_info_filename, 'a') as f:
+                f.write("=====================================\n")
+                f.write("Javascript/QML tracebacks\n")
+                f.write("=====================================\n\n")
+                for idx, stack in enumerate(js_stacks):
+                    f.write(f'JS Stack {idx}:\n{stack}\n\n')
+    except Exception as e:
+        pass
+    
+    print("  - Python/QML crash info saved @ {}".format(more_info_filename))
+    
+c_crashed_callback = None
 
 def init_crash_handling():
+    global c_crashed_callback
     td = os.environ.get("SHOOP_CRASH_DUMP_DIR")
     if not td:
         td = tempfile.gettempdir()
     try:
         import shoopdaloop.shoop_crashhandling
-        shoopdaloop.shoop_crashhandling.shoop_init_crashhandling_with_cb(td, shoopdaloop.shoop_crashhandling.CrashedCallback(crashed_callback))
+        c_crashed_callback = shoopdaloop.shoop_crashhandling.CrashedCallback(crashed_callback)
+        shoopdaloop.shoop_crashhandling.shoop_init_crashhandling_with_cb(td, c_crashed_callback)
     except Exception as e:
         from shoopdaloop.lib.logging import Logger
         logger = Logger('Frontend.CrashHandling')
