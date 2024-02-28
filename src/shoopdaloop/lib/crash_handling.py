de114c4d252566e65ab2e82b96dd72999a038100
Sander Vocke <sandervocke@gmail.com>
2024-02-28 08:46:32 +0100

    wip

--- src/shoopdaloop/lib/crash_handling.py
+++ src/shoopdaloop/lib/crash_handling.py
@@ -7,6 +7,7 @@ import threading
 import time
 
 from PySide6.QtQml import QJSValue
+from PySide6.QtCore import QThread
 from shiboken6 import Shiboken
 
 # Keep track of active javascript engines in order to attempt to get tracebacks from them
@@ -42,25 +43,40 @@ def crashed_callback(filename):
 
     try:
         js_stacks = []
+        failed_to_get_stack_engs = []
+        other_thread_engs = []
         for idx, eng in enumerate(g_active_js_engines):
-            if eng and Shiboken.isValid(eng):                
-                # use an error throwing mechanism to get a stack trace from the engine
-                eng.throwError("Crash handler stack retrieval helper error")
-                err = eng.catchError()
-                if err:
-                    stack = err.property('stack')
-                    if stack:
-                        stack_str = stack.toString()
+            if eng and Shiboken.isValid(eng):
+                if eng.thread() == QThread.currentThread():
+                    stack_str = None
+                    # use an error throwing mechanism to get a stack trace from the engine
+                    eng.throwError("Crash handler stack retrieval helper error")
+                    err = eng.catchError()
+                    if err:
+                        stack = err.property('stack')
+                        if stack:
+                            stack_str = stack.toString()
+                    if stack_str:
                         js_stacks.append(stack_str)
-                        d['finished'] = True
+                    else:
+                        failed_to_get_stack_engs.append(eng)
+                else:
+                    other_thread_engs.append(eng)                
         
-        if len(js_stacks) > 0:
+        if (len(js_stacks) + len(failed_to_get_stack_engs) + len(other_thread_engs)) > 0:
             with open(more_info_filename, 'a') as f:
                 f.write("=====================================\n")
                 f.write("Javascript/QML tracebacks\n")
                 f.write("=====================================\n\n")
-                for idx, stack in enumerate(js_stacks):
-                    f.write(f'JS Stack {idx}:\n{stack}\n\n')
+                idx = 1
+                for stack in enumerate(js_stacks):
+                    f.write(f'Stack for JS engine {idx}:\n{stack}\n\n')
+                    idx += 1
+                for eng in other_thread_engs:
+                    f.write(f'JS engine {idx} is not on the crashing thread - no stack.\n\n')
+                    idx += 1
+                for eng in failed_to_get_stack_engs:
+                    f.write(f'JS engine {idx} is on the crashing thread, but failed to get a stack.\n\n')
     except Exception as e:
         pass
     
