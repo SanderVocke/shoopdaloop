ad6d8d26b9020b0531f4983e6b4d3182a338b380
Sander Vocke <sandervocke@gmail.com>
2024-03-08 22:42:52 +0100

    Nullptr check

--- src/libshoopdaloop/internal/jack/JackAllPorts.cpp
+++ src/libshoopdaloop/internal/jack/JackAllPorts.cpp
@@ -12,14 +12,19 @@ void GenericJackAllPorts<API>::update(jack_client_t *client) {
     for(auto port_name = port_names; port_name != nullptr && *port_name != nullptr; port_name++) {
         auto _port_name = std::string(*port_name);
         auto port = API::port_by_name(client, _port_name.c_str());
+        if (!port) { continue; }
         JackAllPortsEntry entry;
         entry.name = _port_name;
         entry.direction = API::port_flags(port) & JackPortIsInput ? shoop_port_direction_t::Input : shoop_port_direction_t::Output;
-        entry.type = std::string(API::port_type(port)) == JACK_DEFAULT_AUDIO_TYPE ? PortDataType::Audio : PortDataType::Midi;
+        auto port_type_str = API::port_type(port);
+        if (!port_type_str) { continue; }
+        entry.type = std::string(port_type_str) == JACK_DEFAULT_AUDIO_TYPE ? PortDataType::Audio : PortDataType::Midi;
         entry.connections = std::vector<std::string>();
         const char** connected_ports = API::port_get_all_connections(client, port);
         for(auto connected_port = connected_ports; connected_port != nullptr && *connected_port != nullptr; connected_port++) {
-            entry.connections.push_back(std::string(*connected_port));
+            if (connected_port) {
+                entry.connections.push_back(std::string(*connected_port));
+            }
         }
 
         new_cache.push_back(entry);
