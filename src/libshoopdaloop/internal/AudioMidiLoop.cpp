1583a608ae2551a2c5ebd6c301169defe5ff65ff
Sander Vocke <sandervocke@gmail.com>
2024-04-12 16:33:39 +0200

    wip

--- src/libshoopdaloop/internal/AudioMidiLoop.cpp
+++ src/libshoopdaloop/internal/AudioMidiLoop.cpp
@@ -187,56 +187,6 @@ void AudioMidiLoop::PROC_handle_poi() {
     }
 }
 
-void AudioMidiLoop::adopt_ringbuffer_contents(
-    std::shared_ptr<shoop_types::_AudioPort> from_port,
-    std::optional<uint32_t> reverse_start_offset_cycle,
-    std::optional<uint32_t> n_cycles_length,
-    bool thread_safe) {
-    int _n = -1;
-    if (n_cycles_length.has_value()) { _n = (int) n_cycles_length.value(); }
-    auto fn = [this, reverse_start_offset_cycle, n_cycles_length, _n, from_port]() {
-        unsigned reverse_start_offset = 0;
-
-        if (reverse_start_offset_cycle.has_value() && mp_sync_source) {
-            auto len = mp_sync_source->get_length();
-            auto cur_cycle_reverse_start = mp_sync_source->get_position();
-            auto cycle = reverse_start_offset_cycle.value();
-            reverse_start_offset = cur_cycle_reverse_start + len * cycle;
-        } else {
-            // The start offset will just be the smallest ringbuffer size
-            reverse_start_offset = 0;
-            for (auto &channel : mp_audio_channels) {
-                reverse_start_offset = std::max(reverse_start_offset, from_port->get_ringbuffer_n_samples());
-            } 
-        }
-
-        unsigned n_samples = n_cycles_length.has_value() && mp_sync_source ?
-            mp_sync_source->get_length() * n_cycles_length.value() : reverse_start_offset;
-
-        for (auto &channel : mp_audio_channels) {
-            channel->adopt_ringbuffer_contents(from_port, reverse_start_offset, std::nullopt, false);
-        }
-        for (auto &channel : mp_midi_channels) {
-            channel->adopt_ringbuffer_contents(from_port, reverse_start_offset, std::nullopt, false);
-        }
-
-        log<log_level_debug>("Adopting {} ringbuffer samples (calculated from {} cycles) at start offset {}.",
-            n_samples, _n, reverse_start_offset);
-
-        if (n_cycles_length.has_value() && mp_sync_source) {
-            set_length(mp_sync_source->get_length() * n_cycles_length.value(), false);
-        } else {
-            set_length(reverse_start_offset, false);
-        }
-    };
-
-    if (thread_safe) {
-        queue_process_thread_command(fn);
-    } else {
-        fn();
-    }
-}
-
 template std::shared_ptr<AudioChannel<float>> AudioMidiLoop::add_audio_channel(
     std::shared_ptr<ObjectPool<AudioBuffer<float>>> const &buffer_pool,
     uint32_t initial_max_buffers, shoop_channel_mode_t mode, bool thread_safe);
--- src/libshoopdaloop/internal/AudioMidiLoop.h
+++ src/libshoopdaloop/internal/AudioMidiLoop.h
@@ -56,19 +56,6 @@ class AudioMidiLoop : public BasicLoop {
     void PROC_update_poi() override;
 
     void PROC_handle_poi() override;
-
-    // Channels may support constanly running ringbuffers. This function
-    // tells them to take their ringbuffer contents and set them as the
-    // channel data.
-    // @param reverse_start_offset_cycle: Where to set the playback start point in terms
-    //  of sync source cycles. 0 means to use the data since the current cycle
-    //  started, 1 means the start of the last completed cycle, etc.
-    //  nullopt will just use start offset 0.
-    void adopt_ringbuffer_contents(
-        std::shared_ptr<shoop_types::_AudioPort> from_port,
-        std::optional<uint32_t> reverse_start_offset_cycle = std::nullopt,
-        std::optional<uint32_t> n_cycles_length = std::nullopt,
-        bool thread_safe=true);
 };
 
 extern template std::shared_ptr<AudioChannel<float>> AudioMidiLoop::add_audio_channel(
