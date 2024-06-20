d53a8ae20c8d117b250e378fb8ae7e76ac8b0d3a
Sander Vocke <sandervocke@gmail.com>
2024-06-21 00:24:44 +0200

    wip

--- src/libshoopdaloop/internal/lv2/CarlaLV2ProcessingChain.cpp
+++ src/libshoopdaloop/internal/lv2/CarlaLV2ProcessingChain.cpp
@@ -595,15 +595,12 @@ CarlaLV2ProcessingChain<TimeType, SizeType>::~CarlaLV2ProcessingChain() {
 template <typename TimeType, typename SizeType>
 void CarlaLV2ProcessingChain<TimeType, SizeType>::deserialize_state(
     std::string str) {
-    std::cout << "AAA" << std::endl;
     while (!is_ready()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
-    std::cout << "BBB" << std::endl;
     if (!m_state_iface) {
         throw std::runtime_error("No state interface for Carla chain");
     }
-    std::cout << "CCC" << std::endl;
     m_state_restore_active = true;
     std::thread restore_thread([this, str]() {
         try {
@@ -621,8 +618,6 @@ void CarlaLV2ProcessingChain<TimeType, SizeType>::deserialize_state(
         } catch (...) {
             log<log_level_error>("Failed to restore Carla state: unknown exception");
         }
-
-        std::cout << "FFF" << std::endl;
 
         m_state_restore_active = false;
     });
