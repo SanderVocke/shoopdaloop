fb60b9900b59d7678d174300a147058a262c8f2d
Sander Vocke <sandervocke@gmail.com>
2024-09-08 14:10:03 +0200

    wip

--- scripts/gha_bash_with_custom_fail.sh
+++ scripts/gha_bash_with_custom_fail.sh
@@ -13,7 +13,7 @@ echo "GHA wrapper: running command: $BASE_WRAP_SHELL $@"
 $BASE_WRAP_SHELL $@ | tee -a log_all.txt
 
 # check if command was successful
-if [ $? -ne 0 ]; then
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
   echo "Command failed with exit code $?, setting status to failed"
   echo "failure_status=unhandled" | tee -a $GITHUB_ENV
 fi
