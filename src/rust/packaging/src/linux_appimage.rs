313119343fa7692e0547960b4befae8eef64db39
Sander Vocke <sandervocke@gmail.com>
2024-09-13 17:46:20 +0200

    wip

--- src/rust/packaging/src/linux_appimage.rs
+++ src/rust/packaging/src/linux_appimage.rs
@@ -13,31 +13,6 @@ pub fn build_appimage(
 ) -> Result<(), anyhow::Error> {
     let mut src_dir = appdir.to_owned();
 
-
-    println!("Creating temporary directory for stripping...");
-    let tmp_dir : Option<TempDir> = match strip {
-        true => Some(TempDir::new()?),
-        false => None,
-    };
-    if strip {
-        src_dir = tmp_dir.unwrap().path().to_owned().join("appdir");
-
-        std::fs::create_dir_all(&src_dir)
-           .with_context(|| format!("Failed to create {src_dir:?}"))?;
-        recursive_dir_cpy(appdir, &src_dir)
-           .with_context(|| format!("Failed to copy {appdir:?} to {src_dir:?}"))?;
-
-        for file in [
-            "nextest-archive.tar.zst",
-            "cargo-nextest",
-            "shoop_lib/test_runner",
-        ] {
-            let file = src_dir.join(file);
-            std::fs::remove_file(&file)
-                .with_context(|| format!("Failed to remove {file:?}"))?;
-        }
-    }
-
     println!("Creating AppImage...");
     Command::new(appimagetool)
         .args(&[src_dir.to_str().unwrap(), output_file.to_str().unwrap()])
