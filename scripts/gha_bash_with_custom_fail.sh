c671c14f8d08ff0eccb3bbd6dcc6d983677ebfb4
Sander Vocke <sandervocke@gmail.com>
2024-09-27 12:38:44 +0200

    wip

--- scripts/gha_bash_with_custom_fail.sh
+++ scripts/gha_bash_with_custom_fail.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-AND_RESULT=true
+MASK_CMD=/bin/true
 
 if [ "$failure_status" != "" ]; then
   if [[ -n "${GHA_RUN_ALWAYS}" ]]; then
     echo "failure_status is set, but GHA_RUN_ALWAYS is set. Running command."
-    AND_RESULT=false # ensure that the end result fails
+    MASK_CMD=/bin/false # ensure that the end result fails
   else
     echo "failure_status is set"
     exit 1
@@ -13,7 +13,10 @@ fi
 
 echo "GHA wrapper: running command: $BASE_WRAP_SHELL $@"
 $BASE_WRAP_SHELL $@ | tee -a log_all.txt
-RESULT=${PIPESTATUS[0]} && ${AND_RESULT}
+
+# Process the result code
+$(sh -c "exit ${PIPESTATUS[0]}") && $(${MASK_CMD})
+RESULT=$?
 
 # check if command was successful
 if [ ${RESULT} -ne 0 ]; then
@@ -21,3 +24,5 @@ if [ ${RESULT} -ne 0 ]; then
   echo "failure_status=unhandled" | tee -a $GITHUB_ENV
   echo "ACTION_TMATE_CONNECT_TIMEOUT_SECONDS=3600" | tee -a $GITHUB_ENV
 fi
+
+exit $RESULT
