6282378fe3dc426a3e2160f7647fb1a85a338ea4
Sander Vocke <sandervocke@gmail.com>
2024-10-08 11:14:17 +0200

    wip

--- src/rust/shoopdaloop/src/lib.rs
+++ src/rust/shoopdaloop/src/lib.rs
@@ -1,2 +1,5 @@
 #[cfg(not(feature = "prebuild"))]
+mod lib_impl;
+
+#[cfg(not(feature = "prebuild"))]
 pub use lib_impl::*;
--- src/rust/shoopdaloop/src/main.rs
+++ src/rust/shoopdaloop/src/main.rs
@@ -1,4 +1,7 @@
 #[cfg(not(feature = "prebuild"))]
+mod main_impl;
+
+#[cfg(not(feature = "prebuild"))]
 pub use main_impl::*;
 
 #[cfg(feature = "prebuild")]
