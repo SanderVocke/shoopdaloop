6d4c8c3ba92139ead96e4808de91d49d9601e766
Sander Vocke <sandervocke@gmail.com>
2024-11-03 21:43:12 -0300

    wip

--- src/rust/packaging/src/scripts/windows_deps.ps1
+++ src/rust/packaging/src/scripts/windows_deps.ps1
@@ -1,9 +1,16 @@
 $executable = "$args[0]"
+$handled = @()
 $output = & Dependencies.exe -chain -depth 4 $executable.Trim();
 $output | Where-Object { -not ($_ -match "NotFound") } |
     Get-Unique |
     ForEach-Object { " $_" -replace "[|├]", " " -replace "([^ ]+).* : ", "$1" } |
-    ForEach-Object { Write-Output $_ }
+    ForEach-Object {
+        if ($handled -contains $_.Trim()) {
+            return
+        }
+        $handled += $_.Trim()
+        Write-Output $_
+    }
 $output | Where-Object { ($_ -match "NotFound") } |
     Get-Unique |
     ForEach-Object { " $_" -replace "[|├]", " " -replace "([^ ]+).* : ", "$1" } |
