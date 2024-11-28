a2172180a38b27da47545775234234dc69811bb6
Sander Vocke <sandervocke@gmail.com>
2024-11-28 00:47:58 -0300

    feat: Add methods for audio driver management and external port discovery

--- src/rust/backend_bindings/src/audio_driver.rs
+++ src/rust/backend_bindings/src/audio_driver.rs
@@ -29,6 +29,48 @@ impl AudioDriver {
         }
     }
 
+    pub fn start_dummy(&self, settings: &bindings::DummyAudioDriverSettings) -> Result<(), anyhow::Error> {
+        let obj = self.lock();
+        unsafe { ffi::start_dummy_driver(*obj, settings) };
+        Ok(())
+    }
+
+    pub fn start_jack(&self, settings: &bindings::JackAudioDriverSettings) -> Result<(), anyhow::Error> {
+        let obj = self.lock();
+        unsafe { ffi::start_jack_driver(*obj, settings) };
+        Ok(())
+    }
+
+    pub fn active(&self) -> bool {
+        let obj = self.lock();
+        let state = unsafe { ffi::get_audio_driver_state(*obj) };
+        let active = unsafe { (*state).active != 0 };
+        unsafe { ffi::destroy_audio_driver_state(state) };
+        active
+    }
+
+    pub fn wait_process(&self) {
+        let obj = self.lock();
+        unsafe { ffi::wait_process(*obj) };
+    }
+
+    pub fn dummy_run_requested_frames(&self) {
+        let obj = self.lock();
+        unsafe { ffi::dummy_audio_run_requested_frames(*obj) };
+    }
+
+    pub fn find_external_ports(&self, maybe_name_regex: Option<&str>, port_direction: u32, data_type: u32) -> Vec<bindings::ExternalPortDescriptor> {
+        let obj = self.lock();
+        let regex_ptr = maybe_name_regex.map_or(std::ptr::null(), |s| s.as_ptr() as *const i8);
+        let result = unsafe { ffi::find_external_ports(*obj, regex_ptr, port_direction, data_type) };
+        let mut ports = Vec::new();
+        for i in 0..unsafe { (*result).n_ports } {
+            ports.push(unsafe { (*result).ports[i] });
+        }
+        unsafe { ffi::destroy_external_port_descriptors(result) };
+        ports
+    }
+
     pub fn driver_type_supported(driver_type : AudioDriverType) -> bool {
         unsafe { ffi::driver_type_supported(driver_type as u32) != 0 }
     }
