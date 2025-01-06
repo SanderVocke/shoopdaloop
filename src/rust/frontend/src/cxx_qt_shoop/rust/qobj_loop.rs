fb188a1406bbe7439c6d46a989b87de17b4faa45
Sander Vocke <sandervocke@gmail.com>
2025-01-05 22:58:37 -0300

    refactor: Comment out unused channel update logic in Loop implementation

--- src/rust/frontend/src/cxx_qt_shoop/rust/qobj_loop.rs
+++ src/rust/frontend/src/cxx_qt_shoop/rust/qobj_loop.rs
@@ -1,3 +1,6 @@
+use crate::cxx_qt_shoop::qobj_loop_bridge::Loop;                                   
+use crate::cxx_qt_shoop::qobj_loop_bridge::ffi::*;                                           
+use std::pin::Pin;
 
 impl Loop {
     pub fn initialize_impl(self: Pin<&mut Loop>) {
@@ -13,12 +16,12 @@ impl Loop {
         // let audio_chans = self.get_audio_channels_impl();
         // let midi_chans = self.get_midi_channels_impl();
 
-        for channel in &audio_chans {
-            channel.update_on_non_gui_thread();
-        }
-        for channel in &midi_chans {
-            channel.update_on_non_gui_thread();
-        }
+        // for channel in &audio_chans {
+        //     channel.update_on_non_gui_thread();
+        // }
+        // for channel in &midi_chans {
+        //     channel.update_on_non_gui_thread();
+        // }
 
         let prev_position = self.position();
         let prev_mode = self.mode();
@@ -35,9 +38,9 @@ impl Loop {
         self.set_position(state.position as i32);
         self.set_next_mode(state.maybe_next_mode.unwrap_or(state.mode) as i32);
         self.set_next_transition_delay(state.maybe_next_mode_delay.unwrap_or(u32::MAX) as i32);
-        self.set_display_peaks(audio_chans.iter().map(|c| c.output_peak()).collect());
-        self.set_display_midi_notes_active(midi_chans.iter().map(|c| c.n_notes_active()).sum());
-        self.set_display_midi_events_triggered(midi_chans.iter().map(|c| c.n_events_triggered()).sum());
+        // self.set_display_peaks(audio_chans.iter().map(|c| c.output_peak()).collect());
+        // self.set_display_midi_notes_active(midi_chans.iter().map(|c| c.n_notes_active()).sum());
+        // self.set_display_midi_events_triggered(midi_chans.iter().map(|c| c.n_events_triggered()).sum());
 
         if prev_mode != self.mode() {
             self.mode_changed_unsafe(*self.mode());
