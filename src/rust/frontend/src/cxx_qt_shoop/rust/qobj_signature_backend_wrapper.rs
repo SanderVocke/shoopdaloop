197950bcf015508c4f48c5eb263f77adf6e54a3b
Sander Vocke <sandervocke@gmail.com>
2025-01-23 17:31:59 -0300

    Update qobj_signature_backend_wrapper.rs

--- src/rust/frontend/src/cxx_qt_shoop/rust/qobj_signature_backend_wrapper.rs
+++ src/rust/frontend/src/cxx_qt_shoop/rust/qobj_signature_backend_wrapper.rs
@@ -3,5 +3,5 @@ pub mod constants {
 
     pub const SIGNAL_INITIALIZED_CHANGED: &str = "initializedChanged()";
 
-    pub const INVOKABLE_FIND_EXTERNAL_PORTS: &str = "findExternalPorts(QString,int,int)";
+    pub const INVOKABLE_FIND_EXTERNAL_PORTS: &str = "find_external_ports(QString,int,int)";
 }
