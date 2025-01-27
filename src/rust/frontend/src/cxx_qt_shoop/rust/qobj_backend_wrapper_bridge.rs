c3c117c44021fac0eeb91547ef74680bb4eb4a40
Sander Vocke <sandervocke@gmail.com>
2025-01-26 23:55:02 -0300

    feat: Add QString and QVariant types to qobj_backend_wrapper_bridge.rs

--- src/rust/frontend/src/cxx_qt_shoop/rust/qobj_backend_wrapper_bridge.rs
+++ src/rust/frontend/src/cxx_qt_shoop/rust/qobj_backend_wrapper_bridge.rs
@@ -6,6 +6,12 @@ pub mod ffi {
     unsafe extern "C++" {
         include!("cxx-qt-lib-shoop/qquickitem.h");
         type QQuickItem = crate::cxx_qt_lib_shoop::qquickitem::QQuickItem;
+
+        include!("cxx-qt-lib/qstring.h");
+        type QString = cxx_qt_lib::QString;
+
+        include!("cxx-qt-lib/qvariant.h");
+        type QVariant = cxx_qt_lib::QVariant;
     }
 
     unsafe extern "RustQt" {
