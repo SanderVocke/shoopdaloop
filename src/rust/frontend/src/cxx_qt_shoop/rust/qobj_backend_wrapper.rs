c68c47a6cfb21cfdfeea40085c71b3bd2b741fc5
Sander Vocke <sandervocke@gmail.com>
2025-01-27 17:56:44 -0300

    refactor: Update BackendWrapper methods to use Pin for self reference

--- src/rust/frontend/src/cxx_qt_shoop/rust/qobj_backend_wrapper.rs
+++ src/rust/frontend/src/cxx_qt_shoop/rust/qobj_backend_wrapper.rs
@@ -5,81 +5,60 @@ pub use crate::cxx_qt_shoop::qobj_backend_wrapper_bridge::BackendWrapper;
 pub use crate::cxx_qt_shoop::qobj_backend_wrapper_bridge::ffi::make_unique_backend_wrapper as make_unique;
 
 impl BackendWrapper {
-    pub fn register_backend_object(&mut self, _obj: QVariant) {}
+    
+    pub fn register_backend_object(self: Pin<&mut BackendWrapper>, _obj: QVariant);
 
-    pub fn unregister_backend_object(&mut self, _obj: QVariant) {}
-
-    pub fn update_on_gui_thread(&mut self) {}
-
-    pub fn update_on_other_thread(&mut self) {}
-
-    pub fn get_sample_rate(&mut self) -> i32 {
-        0
-    }
-
-    pub fn get_buffer_size(&mut self) -> i32 {
-        0
-    }
-
-    pub fn close(&mut self) {}
-
-    pub fn get_backend_driver_obj(&mut self) -> QVariant {
-        QVariant::default()
-    }
-
-    pub fn get_backend_session_obj(&mut self) -> QVariant {
-        QVariant::default()
-    }
-
-    pub fn dummy_enter_controlled_mode(&mut self) {}
-
-    pub fn dummy_enter_automatic_mode(&mut self) {}
-
-    pub fn dummy_is_controlled(&mut self) -> bool {
-        false
-    }
-
-    pub fn dummy_request_controlled_frames(&mut self, _n: i32) {}
-
-    pub fn dummy_n_requested_frames(&mut self) -> i32 {
-        0
-    }
-
-    pub fn dummy_run_requested_frames(&mut self) {}
-
-    pub fn dummy_add_external_mock_port(&mut self, _name: &str, _direction: i32, _data_type: i32) {}
-
-    pub fn dummy_remove_external_mock_port(&mut self, _name: &str) {}
-
-    pub fn dummy_remove_all_external_mock_ports(&mut self) {}
-
-    pub fn wait_process(&mut self) {}
-
-    pub fn maybe_init(&mut self) {}
-
-    pub fn get_profiling_report(&mut self) -> QVariant {
-        QVariant::default()
-    }
-
-    pub fn backend_type_is_supported(&mut self, _type: i32) -> bool {
-        false
-    }
-
-    pub fn open_driver_audio_port(&mut self, _name_hint: &str, _direction: i32, _min_n_ringbuffer_samples: i32) -> QVariant {
-        QVariant::default()
-    }
-
-    pub fn open_driver_midi_port(&mut self, _name_hint: &str, _direction: i32, _min_n_ringbuffer_samples: i32) -> QVariant {
-        QVariant::default()
-    }
-
-    pub fn segfault_on_process_thread(&mut self) {}
-
-    pub fn abort_on_process_thread(&mut self) {}
-
-    pub fn find_external_ports(&mut self, _maybe_name_regex: &str, _port_direction: i32, _data_type: i32) -> Vec<QVariant> {
-        Vec::new()
-    }
+    pub fn unregister_backend_object(self: Pin<&mut BackendWrapper>, _obj: QVariant);
+    
+    pub fn update_on_gui_thread(self: Pin<&mut BackendWrapper>);
+    
+    pub fn update_on_other_thread(self: Pin<&mut BackendWrapper>);
+    
+    pub fn get_sample_rate(self: Pin<&mut BackendWrapper>) -> i32;
+    
+    pub fn get_buffer_size(self: Pin<&mut BackendWrapper>) -> i32;
+    
+    pub fn close(self: Pin<&mut BackendWrapper>);
+    
+    pub fn get_backend_driver_obj(self: Pin<&mut BackendWrapper>) -> QVariant;
+    
+    pub fn get_backend_session_obj(self: Pin<&mut BackendWrapper>) -> QVariant;
+    
+    pub fn dummy_enter_controlled_mode(self: Pin<&mut BackendWrapper>);
+    
+    pub fn dummy_enter_automatic_mode(self: Pin<&mut BackendWrapper>);
+    
+    pub fn dummy_is_controlled(self: Pin<&mut BackendWrapper>) -> bool;
+    
+    pub fn dummy_request_controlled_frames(self: Pin<&mut BackendWrapper>, _n: i32);
+    
+    pub fn dummy_n_requested_frames(self: Pin<&mut BackendWrapper>) -> i32;
+    
+    pub fn dummy_run_requested_frames(self: Pin<&mut BackendWrapper>);
+    
+    pub fn dummy_add_external_mock_port(self: Pin<&mut BackendWrapper>, _name: &str, _direction: i32, _data_type: i32);
+    
+    pub fn dummy_remove_external_mock_port(self: Pin<&mut BackendWrapper>, _name: &str);
+    
+    pub fn dummy_remove_all_external_mock_ports(self: Pin<&mut BackendWrapper>);
+    
+    pub fn wait_process(self: Pin<&mut BackendWrapper>);
+    
+    pub fn maybe_init(self: Pin<&mut BackendWrapper>);
+    
+    pub fn get_profiling_report(self: Pin<&mut BackendWrapper>) -> QVariant;
+    
+    pub fn backend_type_is_supported(self: Pin<&mut BackendWrapper>, _type: i32) -> bool;
+    
+    pub fn open_driver_audio_port(self: Pin<&mut BackendWrapper>, _name_hint: &str, _direction: i32, _min_n_ringbuffer_samples: i32) -> QVariant;
+    
+    pub fn open_driver_midi_port(self: Pin<&mut BackendWrapper>, _name_hint: &str, _direction: i32, _min_n_ringbuffer_samples: i32) -> QVariant;
+    
+    pub fn segfault_on_process_thread(self: Pin<&mut BackendWrapper>);
+    
+    pub fn abort_on_process_thread(self: Pin<&mut BackendWrapper>);
+    
+    pub fn find_external_ports(self: Pin<&mut BackendWrapper>, _maybe_name_regex: &str, _port_direction: i32, _data_type: i32) -> Vec<QVariant>;
 }
 
 #[cfg(test)]
