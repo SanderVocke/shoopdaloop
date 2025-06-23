6e48fc61db8ac4f5fbfc4e7b4ba4903fff6d43c7
Sander Vocke <sandervocke@gmail.com>
2025-06-23 20:03:12 +0200

    wip

--- src/backend/internal/BasicLoop.cpp
+++ src/backend/internal/BasicLoop.cpp
@@ -103,7 +103,7 @@ void BasicLoop::PROC_handle_poi() {
         changed = true;
     }
 
-    if (mp_next_poi->type_flags == 0) {
+    if (mp_next_poi && mp_next_poi->type_flags == 0) {
         mp_next_poi = std::nullopt;
         changed = true;
     }
