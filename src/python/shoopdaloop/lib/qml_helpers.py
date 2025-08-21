3b56aa2b641fcd5eba81cacfabff2e9129cd0360
Sander Vocke <sandervocke@gmail.com>
2025-08-21 22:39:18 +0200

    wip

--- src/python/shoopdaloop/lib/qml_helpers.py
+++ src/python/shoopdaloop/lib/qml_helpers.py
@@ -18,8 +18,6 @@ pkg_version = shoop_version
 from .q_objects.LoopAudioChannel import LoopAudioChannel
 from .q_objects.LoopMidiChannel import LoopMidiChannel
 from .q_objects.ClickTrackGenerator import ClickTrackGenerator
-from .q_objects.Task import Task
-from .q_objects.Tasks import Tasks
 from .q_objects.FetchChannelData import FetchChannelData
 from .q_objects.FileIO import FileIO
 from .q_objects.KeyModifiers import KeyModifiers
@@ -80,8 +78,6 @@ def register_shoopdaloop_qml_classes():
     register_qml_class(LoopAudioChannel, 'LoopAudioChannel')
     register_qml_class(LoopMidiChannel, 'LoopMidiChannel')
     register_qml_class(ClickTrackGenerator, 'ClickTrackGenerator')
-    register_qml_class(Task, 'Task')
-    register_qml_class(Tasks, 'Tasks')
     register_qml_class(FetchChannelData, 'FetchChannelData')
     register_qml_class(FileIO, 'FileIO')
     register_qml_class(KeyModifiers, 'KeyModifiers')
