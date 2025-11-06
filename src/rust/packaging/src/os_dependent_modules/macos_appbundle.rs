30a66aed9bfba7ff8c5ce13793acac6f97e2af6c
Sander Vocke <sandervocke@gmail.com>
2025-11-06 22:32:49 +0100

    wip

--- src/rust/packaging/src/os_dependent_modules/macos_appbundle.rs
+++ src/rust/packaging/src/os_dependent_modules/macos_appbundle.rs
@@ -51,17 +51,22 @@ fn populate_appbundle(appdir: &Path, exe_path: &Path) -> Result<(), anyhow::Erro
     let runtime_dir = appdir.join("runtime");
     std::fs::create_dir(&runtime_dir)
         .with_context(|| format!("Cannot create dir: {:?}", runtime_dir))?;
-    recursive_dir_cpy(
-        &PathBuf::from(runtime_env_dir),
-        &runtime_dir
-    )?;
+    recursive_dir_cpy(&PathBuf::from(&runtime_dir), &runtime_dir)?;
 
     info!("Getting dependencies (this may take some time)...");
     let dynlib_dir = appdir.join("lib");
     let excludelist_path = src_path.join("distribution/macos/excludelist");
     let includelist_path = src_path.join("distribution/macos/includelist");
-    let final_exe_dir = final_exe_path.parent().ok_or(anyhow::anyhow!("Could not get executable directory"))?;
-    let libs = get_dependency_libs (&final_exe_path, &final_exe_dir, &excludelist_path, &includelist_path, true)?;
+    let final_exe_dir = exe_path
+        .parent()
+        .ok_or(anyhow::anyhow!("Could not get executable directory"))?;
+    let libs = get_dependency_libs(
+        &exe_path,
+        &final_exe_dir,
+        &excludelist_path,
+        &includelist_path,
+        true,
+    )?;
 
     info!("Bundling dependencies...");
     std::fs::create_dir(&dynlib_dir)?;
@@ -87,11 +92,11 @@ fn populate_appbundle(appdir: &Path, exe_path: &Path) -> Result<(), anyhow::Erro
         } else if std::fs::metadata(&from)?.is_dir() {
             info!("  Bundling {}", lib.to_str().unwrap());
             recursive_dir_cpy(&from, &to)
-               .with_context(|| format!("Failed to copy dir {from:?} to {to:?}"))?;
+                .with_context(|| format!("Failed to copy dir {from:?} to {to:?}"))?;
         } else {
             info!("  Bundling {}", lib.to_str().unwrap());
             std::fs::copy(&from, &to)
-               .with_context(|| format!("Failed to copy {from:?} to {to:?}"))?;
+                .with_context(|| format!("Failed to copy {from:?} to {to:?}"))?;
         }
     }
 
