b0ca9b7b29d80a1ef3c3c9e7088cee613123babe
Sander Vocke <sandervocke@gmail.com>
2026-05-20 21:31:50 +0200

    wip

--- src/rust/backend_rust/src/lib.rs
+++ src/rust/backend_rust/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod audio_buffer_queue;
 pub mod audio_port;
 pub mod audio_port_cxx;
-pub mod dummy_audio_midi_driver;
-pub mod dummy_audio_midi_driver_cxx;
 pub mod decoupled_midi_port;
 pub mod decoupled_midi_port_cxx;
+pub mod dummy_audio_midi_driver;
+pub mod dummy_audio_midi_driver_cxx;
 pub mod dummy_audio_port;
 pub mod dummy_audio_port_cxx;
 pub mod dummy_external_connections;
